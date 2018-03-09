6df2abb73c13ede8a6b5c4170048d199a901a6c6
0pGam3rMC <kuasir321@gmail.com>
2018-03-08 19:04:39 -0500

    Update and rename src/CLACore/Loader.php to src/SkyRealm/Core.php

--- src/SkyRealm/Core.php
+++ src/SkyRealm/Core.php
@@ -1,43 +1,18 @@
 <?php
 
-/*
- * CLACore, a public core with many features for PocketMine-MP
- * Copyright (C) 2017-2018 CLADevs
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 declare(strict_types=1);
 
-namespace CLACore;
+namespace SkyRealm;
 
-use CLACore\Events\PlayerChat;
-use CLACore\Commands\CommandManager;
+use SkyRealm\Events\PlayerChat;
+use SkyRealm\Commands\CommandManager;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\{Config, TextFormat as C};
 
 class Loader extends PluginBase{
 
-    public $prefix = "
-      _____ _               _____
-     / ____| |        /\   / ____|
-    | |    | |       /  \ | |     ___  _ __ ___
-    | |    | |      / /\ \| |    / _ \| '__/ _ \
-    | |____| |____ / ____ \ |____ (_) | | |  __/
-     \_____|______/_/    \_\_____\___/|_|  \___|
-     ";
+    public $prefix = "SkyRealmCore";
 
     private static $instance;
 
@@ -55,6 +30,7 @@ class Loader extends PluginBase{
     }
 
     public function onEnable(){
+        $this-getLogger->info("SkyRealm's Core Plugin has been successfully installed");
         $this->RegisterConfig();
         $this->RegisterManager();
         $this->RegisterEvents();
